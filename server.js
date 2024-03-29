b14d9f1a5529d95905af9202dd041d425b0fca66
Nate Johnson <njohnson2897@gmail.com>
2024-03-28 21:22:33 -0500

    adds post route to /api/notes that successfully appends new notes to db.json file

--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 const express = require('express');
 const path = require('path');
 const { readFromFile, writeToFile, readAndAppend } = require('./helpers/fsUtils.js')
+const { v4: uuidv4 } = require('uuid');
 
 // PORT optionality allows app to be run locally or deployed on a platform 
 const PORT = process.env.port || 3001; 
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
 // GET route for /notes that delivers notes.html
@@ -19,10 +22,31 @@ app.get('/notes', (req, res) =>
 app.get('/api/notes', (req, res) =>
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data))));
 
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json("Successfully recorded the note");
+    } else {
+        res.error('Please include a title and text for your note');
+    };
+});
+
+
 // wildcard GET route that delivers index.html
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
 
 
 
