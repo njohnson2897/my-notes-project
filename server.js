2d6f5e57c71cc1f959cbff91ffca2c932303beb1
Nate Johnson <njohnson2897@gmail.com>
2024-03-28 19:21:11 -0500

    adds GET routes for homepage and notes page

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
-
-
-
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 
 app.listen(PORT, () =>
