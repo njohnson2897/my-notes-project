abbc030d5f84a1fe6c44f539f3039b0e1b064610
Nate Johnson <njohnson2897@gmail.com>
2024-03-28 20:30:12 -0500

    adds commentary

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const util = require('util');
 
+// got this syntax from the module 11 mini project
 const readFromFile = util.promisify(fs.readFile);
 
 
--- server.js
+++ server.js
@@ -1,21 +1,25 @@
+// imports the necessary modules and functions
 const express = require('express');
 const path = require('path');
 const { readFromFile } = require('./helpers/fsUtils.js')
 
-
+// PORT optionality allows app to be run locally or deployed on a platform 
 const PORT = process.env.port || 3001; 
 
 const app = express();
 
 app.use(express.static('public'));
 
+// GET route for /notes that delivers notes.html
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// GET route for /api/notes that reads from db.json
 app.get('/api/notes', (req, res) =>
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data))));
 
+// wildcard GET route that delivers index.html
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
