353f6456c0ef3ff2b8fc5677d6117bf49a1ce6fc
Nate Johnson <njohnson2897@gmail.com>
2024-03-29 09:24:39 -0500

    adds delete route and accesses ID of selected note

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 // imports the necessary modules and functions
 const express = require('express');
 const path = require('path');
-const { readFromFile, writeToFile, readAndAppend } = require('./helpers/fsUtils.js')
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils.js')
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs')
 
 // PORT optionality allows app to be run locally or deployed on a platform 
 const PORT = process.env.port || 3001; 
 
 const app = express();
 
+// middleware to be able to POST JSON and form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
@@ -26,7 +28,7 @@ app.post('/api/notes', (req, res) => {
     console.log(req.body);
 
     const { title, text } = req.body;
-
+// if the request body exists, create a new note using the body, give it an id, and add it to db.json
     if (req.body) {
         const newNote = {
             title,
@@ -39,6 +41,24 @@ app.post('/api/notes', (req, res) => {
     } else {
         res.error('Please include a title and text for your note');
     };
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    // const notes = fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    //     if(err) {
+    //         console.log(err)
+    //      } else {
+    //         for (let i=0; i<data.length; i++) {
+    //             if (data[i].note_id === id) {
+    //                 data.splice(i, 1);
+    //             };
+    //         };
+    //      };
+    // });
+    
+    res.json("Successfully deleted the specified post.")
 });
 
 
